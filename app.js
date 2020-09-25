99dae0d7873a6e6372337ec81862f6890119eafc
Sheridan McKisick <mcksheridan@gmail.com>
2020-09-25 17:57:26 -0500

    Removed 'users' route

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/bookmarks', bookmarksRouter);
 
 // catch 404 and forward to error handler
