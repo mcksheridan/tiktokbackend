36d2fc73fef2cc715623baf38f8a2d2a18889833
Sheridan McKisick <sheridan@heysparkbox.com>
2021-03-25 13:06:49 -0400

    Update database settings for SSL

--- db/index.js
+++ db/index.js
@@ -1,5 +1,10 @@
 const { Pool } = require('pg')
-const pool = new Pool()
+const pool = new Pool({
+  ssl: true,
+  ssl: {
+  rejectUnauthorized: false,
+  }
+})
 module.exports = {
     query: (text, params, callback) => {
         return pool.query(text, params, callback)
