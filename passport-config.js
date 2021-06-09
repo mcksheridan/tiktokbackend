7af4a8e4b5058eff6c846924e96f3f4050b47f9b
Sheridan McKisick <sheridan@heysparkbox.com>
2021-06-09 14:44:32 -0400

    Style: Remove linter warnings from passport config
    ESLint is fairly new to this project, so I am beginning by cleaning up
    almost 100 linter errors that were in this file. All errors were auto fixable,
    so no significant refactoring took place.
    
    FSA2021-182

--- passport-config.js
+++ passport-config.js
@@ -1,59 +1,56 @@
-const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const db = require('./db')
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const db = require('./db');
 
 function initializePassport() {
-    passport.use(new LocalStrategy({
-        usernameField: 'email',
-        session: true,
-    },
-    function(username, password, done) {
-        const findUser = async () => {
-            const text = 'SELECT * FROM users WHERE EMAIL = $1'
-            const values = [username]
-            try {
-                const userQuery = await db.query(text, values)
-                const user = userQuery.rows[0]
-                if (!user) {
-                    return done(null, false, { message: 'Email not found.' })
-                }
-                try {
-                    if (await bcrypt.compare(password, user.password)) {
-                        return done(null, user)
-                    } else {
-                        return done(null, false, { message: 'Incorrect password.' })
-                    }
-                }
-                catch(error) {
-                    console.log(error)
-                    return done(error)
-                }
-            }
-            catch(error) {
-                console.log(error)
-                return done(error)
-            }
+  passport.use(new LocalStrategy({
+    usernameField: 'email',
+    session: true,
+  },
+  ((username, password, done) => {
+    const findUser = async () => {
+      const text = 'SELECT * FROM users WHERE EMAIL = $1';
+      const values = [username];
+      try {
+        const userQuery = await db.query(text, values);
+        const user = userQuery.rows[0];
+        if (!user) {
+          return done(null, false, { message: 'Email not found.' });
         }
-        findUser()
-    }))
-    passport.serializeUser((user, done) => {
-        done(null, user.user_id)
-    })
-    passport.deserializeUser((id, done) => {
-        const findUser = async () => {
-            const text = 'SELECT user_id FROM users WHERE user_id = $1'
-            const values = [id]
-            try {
-                const userQuery = await db.query(text, values)
-                const user = userQuery.rows[0]
-                done(null, user)
-            } catch(error) {
-                done(null, false, {error: error})
-            }
+        try {
+          if (await bcrypt.compare(password, user.password)) {
+            return done(null, user);
+          }
+          return done(null, false, { message: 'Incorrect password.' });
+        } catch (error) {
+          console.log(error);
+          return done(error);
         }
-        findUser()
-    })
+      } catch (error) {
+        console.log(error);
+        return done(error);
+      }
+    };
+    findUser();
+  })));
+  passport.serializeUser((user, done) => {
+    done(null, user.user_id);
+  });
+  passport.deserializeUser((id, done) => {
+    const findUser = async () => {
+      const text = 'SELECT user_id FROM users WHERE user_id = $1';
+      const values = [id];
+      try {
+        const userQuery = await db.query(text, values);
+        const user = userQuery.rows[0];
+        done(null, user);
+      } catch (error) {
+        done(null, false, { error });
+      }
+    };
+    findUser();
+  });
 }
 
-module.exports = initializePassport
+module.exports = initializePassport;
